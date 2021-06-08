e4371bba825494248e24b115f4d01d8f90d2b0a2
ZizhaoWa <81192@stu.sjzu.edu.cn>
2021-06-08 20:52:43 +0800

    This pro is established by Clion 2020.3 is used to for algorithm implementation！
    First submission！

---
+++ Algorithm implementation/cmake-build-debug/CMakeFiles/Algorithm_implementation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Algorithm_implementation.exe"
+  "Algorithm_implementation.exe.manifest"
+  "Algorithm_implementation.pdb"
+  "CMakeFiles/Algorithm_implementation.dir/main.cpp.obj"
+  "libAlgorithm_implementation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Algorithm_implementation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
